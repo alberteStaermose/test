695a08dcc3805f038bd1d57131c17a0d1ce23952
alberteStaermose <55623@dmjx.dk>
2025-02-12 16:09:40 +0100

    about og css

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,47 @@
+<script>
+// import Kontakt from "../components/Kontakt.vue";
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body class="grid gap-24">
+    <section class="flex items-end h-[40vh] px-32 justify-center bg-[#04151E]">
+      <h1 class="text-white mb-8">Alberte Stærmose</h1>
+    </section>
+
+    <section class="grid grid-cols-2 px-56 gap-x-20 gap-y-16">
+      <div>
+        <h2>Hvem er jeg?</h2>
+        <p>
+          Mit navn er Alberte Stærmose. Jeg er uddannet Multimediedesigner fra
+          KEA - Københavns Erhvervsakademi, og studerer lige nu Coded Design på
+          DMJX - Danamarks Medie- og Journalisthøjskole. Som digital designer
+          arbejder jeg altid struktureret og konsekvent. Jeg kan arbejde
+          selvstændigt, men er også en teamplayer. Med min uddannelse som
+          multimediedesigner kan jeg kode, lave grafisk materiale, tage billeder
+          og lave video til hjemmesider mm. Alt laves til rette målgruppe, med
+          et visuelt design, der passer til den visuelle identitet. Med Coded
+          Design har jeg, og udvider fortsat min kode erfaring. Jeg arbejder med
+          både processing og frontend-kode og kan bruge koden aktivt og
+          kreativt. Udover kode har jeg erfaring med adobepakken og Blender.
+        </p>
+      </div>
+
+      <div>
+        <!-- <img src="/media/A.svg" alt="" /> -->
+      </div>
+
+      <!-- <div><img src="/media/mig.jpg" alt="" /></div> -->
+      <div>
+        <h2>Interesseret?</h2>
+      </div>
+    </section>
+
+    <Kontakt></Kontakt>
+  </body>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+svg {
+  fill: blue;
 }
 </style>
