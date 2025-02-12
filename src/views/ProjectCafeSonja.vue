7adf55aa4587aee702be1e07665ce49e51305634
alberteStaermose <55623@dmjx.dk>
2025-02-12 17:23:10 +0100

    lavet intro videre på sonja

--- src/views/ProjectCafeSonja.vue
+++ src/views/ProjectCafeSonja.vue
@@ -2,23 +2,53 @@
 
 <template>
   <main>
-    <section>
-      <h1>Café Sonja</h1>
+    <section class="h-screen bg-[#04151E] sticky top-0 -z-20 w-screen">
+      <h1>HERO</h1>
     </section>
-    <section class="grid grid-cols-4 p-20 gap-3">
+    <section class="bg-white grid grid-cols-2 p-20">
+      <h1>Café Sonja</h1>
+      <div class="justify-self-end">
+        <div class="flex items-baseline gap-1">
+          <h3>Projekt:</h3>
+          <h4>Skoleprojekt</h4>
+        </div>
+        <div class="flex items-baseline gap-1">
+          <h3>Min rolle:</h3>
+          <h4>Skoleprojekt</h4>
+        </div>
+        <div class="flex items-baseline gap-1">
+          <h3>Kunden:</h3>
+          <h4>Skoleprojekt</h4>
+        </div>
+      </div>
+    </section>
+    <section class="grid grid-cols-3 p-20 gap-10 bg-white">
       <div>
         <h2>Brief</h2>
+        <p>
+          Vi ønsker at opfriske Café Sonja brandet. Café Sonja har eksisteret i
+          over 30 år og har været mange forskellige ting igennem tiderne. Nu har
+          vi fundet et cafékoncept, som er populært og nu skal vi fortælle endnu
+          mere om det og styrke vores brand. Café Sonja har sit navn efter
+          Tv-serien Sonja i Saxogade og det fylder nogle gange lidt for meget,
+          så derfor vil vi gerne frigøre os fra den historien uden at glemme det
+          helt – så vi ønsker ikke at skifte navn, men få sat nogle flere
+          historie på hvad Café Sonja er.
+          <br />
+          Café Sonja skal være i top 3, når man tænker på klassiske cafeer i
+          København. Vi er en af de første cafeer i KBH, så vi skal være mere
+          kendte. Vi ønsker, at vi med et styrket brand, kan vise alt det Sonja
+          er og tiltrække flere gæster og turister. Sonja er en del af det store
+          sociale arbejde der bliver lavet i Saxogade og det må meget gerne
+          skinne igennem i brandet, samtidigt med at vi stadig skal fremstå som
+          en up-to-date café i kbh.
+        </p>
       </div>
       <div>
         <h2>Proces</h2>
       </div>
       <div>
         <h2>Indsigt og løsning</h2>
-      </div>
-      <div>
-        <h3>Projekt: Skoleprojekt</h3>
-        <h3>Min rolle</h3>
-        <h3>Kunden</h3>
       </div>
     </section>
   </main>
