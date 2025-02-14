cd4a04144e4da7c4711822d1a3ce061e68c972b2
alberteStaermose <55623@dmjx.dk>
2025-02-14 23:15:52 +0100

    tilføjet plakat og mere tekst

--- src/views/ProjectWhileInBattle.vue
+++ src/views/ProjectWhileInBattle.vue
@@ -6,7 +6,7 @@
       <img class="w-screen" src="/media/whileSite.webp" alt="" />
     </section>
     <section class="bg-white grid grid-cols-2 p-20">
-      <h1>Valør</h1>
+      <h1>While In Battle</h1>
       <div class="justify-self-end">
         <div class="flex items-baseline gap-1">
           <h3>Projekt:</h3>
@@ -14,15 +14,15 @@
         </div>
         <div class="flex items-baseline gap-1">
           <h3>Type:</h3>
-          <h4>Individuel opgave</h4>
+          <h4>Gruppeopgave</h4>
         </div>
         <div class="flex items-baseline gap-1">
           <h3>Kunden:</h3>
-          <h4>Møbelbutik</h4>
+          <h4>Metropolis</h4>
         </div>
         <div class="flex items-baseline gap-1">
           <h3>Min rolle:</h3>
-          <h4>Brand designer og projektleder</h4>
+          <h4>Projektleder, grafisk og coded designer</h4>
         </div>
       </div>
     </section>
@@ -30,49 +30,75 @@
       <div>
         <h2>Clientbrief</h2>
         <p>
-          Dette eksamensprojekt gik ud på at opsætte en visuel identitet,
-          animere et navnetræk, samt lave en asset generator til en fiktiv
-          kunde. Vi fik tildelt en type af virksomhed ved lodtrækning og jeg
-          trak møbelbutik.
+          Dette eksamensprojekt gik ud på at vi skulle lave en visuel identitet,
+          et kampagnesite, en plakat og en fysisk installation til
+          danseferestillingen "While In Battle I'm Free, Never Free To Rest" som
+          blev opført af Metropolis og dansekompagniet Culberg på Blågårdsplads
+          og var koreograferet af Hooman Sharifi.
         </p>
       </div>
       <div>
-        <h2>Proces, insight og løsning</h2>
+        <h2>Proces</h2>
         <p>
-          Jeg startede dette projekt ud med at lægge en struktureret plan for
-          min process, hvor jeg her benyttede mig af Trello. Jeg tog et valg om
-          at møbelbutikken skulle være en high end møbelforretning og gik så i
-          gang med at lave en dybdegående research på konkurrenter, trends og
-          tendenser til at finde på et koncept og udvikle en visuel identitet
-          til møbelbutikken. Med en nøje udvalgt farvepalette gik jeg i gang med
-          at lave illustrationer og generere billeder i Midjourney. Til sidst
-          kodede jeg det animerede navnetræk og udviklede en asset generator i
-          Processing.
+          Vi startede dette projekt ud med at lægge en struktureret plan for
+          vores process, hvor vi her benyttede os af Trello til dette. Det var
+          vigtgit for os at lave en dybdegående research på forestillingen og
+          Hooman, og de valg han tog omkring forestillingen, for virkelig forstå
+          budskabet bag danseforestillingen. Baseret på denne dybdegående
+          research og analyse af Metropolis’ vision, stykkets tematik, aktører
+          mm. designede vi en visuel identitet til forestillingen. Med afsæt i
+          den visuelle identitet designede vi en plakat samt et kampagnesite der
+          annoncerer og kommunikerer forestillingen. Til sidst designede og
+          kodede vi i processing en fysisk installation til selve forestillingen
+          i som tilskuerne kan interagere med, der udfolder den visuelle
+          identitet, en del af forestillingen vi gerne ville belyse og vækker
+          nysgerrighed.
         </p>
+        <!-- <ul>
+          <li>
+            Installationen skal folde din visuelle identitet ud og kommunikere
+            den del af forestillingen du gerne vil belyse.
+          </li>
+          <li>
+            Installationen skal vække nysgerrighed, være informerende og
+            forklarende.
+          </li>
+          <li>
+            Installationen skal indeholde en lydside men skal også kunne fungere
+            uden lyd.
+          </li>
+        </ul> -->
       </div>
       <div>
         <h2>Insight og løsning</h2>
         <p class="mb-3">
-          Ud fra min research kom jeg frem til at en stor trend indenfor
-          boligindretning var farver og leg og jeg baserede derfor konceptet og
-          navnet ud fra dette og navngav butikken ud fra fagtermet "Valør".
-          Tager man et kig på konkurrenternes billeder, ser man en visualisering
-          af et perfekt opryddet hjem. Næsten for perfekt, og man kunne blive i
-          tvivl om der overhovedet er nogen der bor sådan. Dette gør Valør op
-          med. Derfor er Valør en møbelbutik der sælger dansk møbeldesign med
-          fokus på farver, og på at sælge et realistisk billede af
-          boligindretningen. Den visuelle identitet består af en farverpalette,
-          illustrationer og et animeret navnetræk. Jeg udviklede en visuel
-          identitet til Valør fuld af farver og leg og lavede en assetganarator
-          hvis hovedformål er at generere forskellige mønstre til den visuelle
-          identitet ved at stille på forskellige paramtre. Asset generatoren
-          gjorde det muligt at udvide den grafiske palette nemt og hurtigt
-          baseret på få grafiske elementer.
+          Vores visuelle identitet er skabt ud fra værdiord – abstraktion,
+          bevægelse og kontrast. Centralt i vores visuelle identitet er en
+          grafisk konstruktion af abstrakte former, som danner et samlet
+          billede. Det var vigtigt for os at inddrage abstraktion, for at
+          appellere til en af egenskaberne contemporary art har, nemlig at
+          invitere beskueren til en personlig refleksion, og ikke give et
+          endegyldigt svar på hvordan det skal tolkes. Vores andet værdiord er
+          bevægelse. Kroppen som dynamisk form er centralt, selvsagt fordi det
+          er en danseforestilling, men også for at fremhæve tematikken, at
+          danserne mødes og taler et fælles sprog igennem dansen. Formerne skal
+          skabe associationer til dansende kropsdele, men samtidig åbne op for
+          egen tolkning. Det sidste værdiord er kontrast. Kontrasten mellem
+          fællesskabet og individet, krig og fred og en legende dansefest og et
+          dramatisk opgør mellem mennesker. Vi har brugt kontrasten til at skabe
+          dramatik i mødet mellem bløde dynamiske former, en grotesk font og i
+          farverne.
         </p>
       </div>
     </section>
     <section class="pl-40 pb-20 pr-40 bg-white gap-20 grid">
       <article class="grid gap-20">
+        <img
+          class="w-1/3 place-self-center"
+          src="/media/whilePlakat.webp"
+          alt=""
+        />
+
         <video controls>
           <source src="/valorVideo.mp4" type="video/mp4" />
 
@@ -81,10 +107,6 @@
         <img class="w-full" src="/media/valorLogoPosNeg.webp" alt="" />
         <img class="w-full" src="/media/valorComputer.webp" alt="" />
         <img class="w-full" src="/media/valorSite.webp" alt="" />
-        <div class="grid gap-2">
-          <img class="w-full" src="/media/valorBilleder1.webp" alt="" />
-          <img class="w-full" src="/media/valorBilleder2.webp" alt="" />
-        </div>
       </article>
       <article class="grid grid-cols-2 gap-20">
         <img class="w-full" src="/media/valorBoxes.webp" alt="" />
