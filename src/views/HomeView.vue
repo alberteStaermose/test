ea4726186ee3e06302ce636567867a61743c06f9
alberteStaermose <55623@dmjx.dk>
2025-02-14 01:32:10 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,14 +8,22 @@ import HeroSection from "../components/HeroSection.vue";
     <HeroSection />
     <section>
       <article class="grid grid-cols-3">
-        <RouterLink to="/pSonja"
-          ><div class="projectView"><p>Café Sonja</p></div></RouterLink
-        >
         <RouterLink to="/pSonja">
-          <div class="projectView"><p>Valør</p></div></RouterLink
-        >
+          <div class="projectView">
+            <p class="hidden">Café Sonja</p>
+            <img class="projectView" src="/media/sonjaLogo.webp" alt="" />
+          </div>
+        </RouterLink>
+        <RouterLink to="/pValor">
+          <div class="projectView">
+            <p class="hidden">Valør</p>
+            <img class="projectView" src="/media/sonjaLogo.webp" alt="" />
+          </div>
+        </RouterLink>
         <RouterLink to="/pPolitiken"
-          ><div class="projectView"><p>Politiken Plus test</p></div>
+          ><div class="projectView">
+            <img src="/media/politikenLogo.webp" alt="" />
+          </div>
         </RouterLink>
         <RouterLink to="/pSonja"
           ><div class="projectView"><p>While in battle</p></div></RouterLink
@@ -45,22 +53,32 @@ import HeroSection from "../components/HeroSection.vue";
 
 <style scoped>
 .projectView {
-  height: 400px;
+  height: 250px;
   background-color: rgb(178, 178, 226);
-  padding-left: 18px;
-  padding-top: 10px;
+}
+.projectView img {
+  width: 100%;
 }
 .projectView p {
+  display: none;
+}
+.projectView:hover p {
   /* display: none; */
   display: block;
 }
 .projectView:hover {
-  height: 400px;
+  height: 250px;
+  /* display: none; */
   background-color: blue;
+}
+.projectView:hover img {
+  display: none;
 }
 
 .projectView:hover p {
   display: block;
   font-size: 20px;
+  padding-left: 10px;
+  padding-top: 10px;
 }
 </style>
