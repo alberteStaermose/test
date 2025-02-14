ab93e2682eddd90beacf2fac9458d0561177a398
alberteStaermose <55623@dmjx.dk>
2025-02-14 01:57:12 +0100

    fjernet valor igen

--- src/router/index.js
+++ src/router/index.js
@@ -33,14 +33,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/ProjectPolitikenplus.vue"),
     },
-    {
-      path: "/pValor",
-      name: "pValor",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/ProjectValor.vue"),
-    },
+    // {
+    //   path: "/pValor",
+    //   name: "pValor",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/ProjectValor.vue"),
+    // },
   ],
 });
 
