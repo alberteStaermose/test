78a9d1d5b616b2d62aee28ac57700d463a30aa9a
alberteStaermose <55623@dmjx.dk>
2025-02-19 20:24:14 +0100

    forzøgt med cirklervsom baggrund

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,34 +1,15 @@
+<script setup></script>
+
 <template>
   <section class="relative overflow-hidden">
     <div class="grid lg:grid-cols-2 lg:h-screen h-screen">
-      <div class="z-10 grid place-content-center bg-white/50 lg:bg-[#04151E]">
+      <div class="z-10 grid place-content-center bg-[#04151E]">
         <div class="p-20 text-white">
-          <!-- <img class="" src="/public/media/DD.svg" alt="" /> -->
           <h1>Alberte STærmose</h1>
           <p>Creative Designer</p>
         </div>
       </div>
-
-      <!-- <video
-        autoplay
-        loop
-        muted
-        class="absolute lg:relative top-0 overflow-hidden z-0 min-h-full w-full self-center"
-      >
-        <source
-          class="lg:fixed absolute"
-          src="/media/circlerNy.mp4"
-          type="video/mp4"
-        />
-      </video> -->
     </div>
-
-    <!-- <div class="bg-rose-300">
-      <video class="object-none w-fit h-screen">
-        <source src="./img/tuborhMandVideo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div> -->
   </section>
 </template>
 
