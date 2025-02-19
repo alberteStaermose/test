e06447170f3697056fbb2b798640a999c32197a1
alberteStaermose <55623@dmjx.dk>
2025-02-19 13:26:22 +0100

    tidslinje på om mig

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,56 +1,89 @@
-<script>
-// import Kontakt from "../components/Kontakt.vue";
-</script>
+<script></script>
 
 <template>
-  <body class="grid gap-24">
+  <body class="grid gap-24 mb-20">
     <section class="flex items-end h-[40vh] px-32 justify-center bg-[#04151E]">
       <h1 class="text-white mb-8">Alberte Stærmose</h1>
     </section>
 
     <section class="grid grid-cols-2 px-56 gap-x-20 gap-y-16">
-      <div>
-        <h2>Hvem er jeg?</h2>
-        <p>
-          Mit navn er Alberte Stærmose. Jeg er uddannet Multimediedesigner fra
-          KEA - Københavns Erhvervsakademi, og studerer lige nu Design og
-          Business på studieretningen Creative Branding på KEA - Københavns
-          Erhvervsakademi. Som kreativ designer arbejder jeg altid struktureret
-          og konsekvent. Jeg er en teamplayer, men kan også arbejde
-          selvstendigt. Mulitmediedesign har givet mig kompetencer indenfor
-          hjemmesideopsætning, UX, UI og kode, hvor jeg på Creative Branding har
-          fået kompetencer indenfor det strategiske og det kreative omkring
-          brandingprocessen, kampagner, kampagnestrategi, brandstrategi,
-          visuelle og verbale identiteter og aktivering af det på kanaler og
-          platforme. Jeg går til alle projekter velplanlagt og struktureret og
-          alt udvikles og baseres på dybdegående research.
-        </p>
+      <div class="grid gap-10">
+        <div>
+          <h2>Hvem er jeg?</h2>
+          <p>
+            Mit navn er Alberte Stærmose. Jeg er uddannet Multimediedesigner fra
+            KEA - Københavns Erhvervsakademi, og studerer lige nu Design og
+            Business på studieretningen Creative Branding på KEA - Københavns
+            Erhvervsakademi. Som kreativ designer arbejder jeg altid
+            struktureret og konsekvent. Jeg er en teamplayer, men kan også
+            arbejde selvstendigt. Mulitmediedesign har givet mig kompetencer
+            indenfor hjemmesideopsætning, UX, UI og kode, hvor jeg på Creative
+            Branding har fået kompetencer indenfor det strategiske og det
+            kreative omkring brandingprocessen, kampagner, kampagnestrategi,
+            brandstrategi, visuelle og verbale identiteter og aktivering af det
+            på kanaler og platforme. Jeg går til alle projekter velplanlagt og
+            struktureret og alt udvikles og baseres på dybdegående research.
+          </p>
+        </div>
+        <div class="grid">
+          <h2 class="">Mine kompetencer</h2>
+          <ul class="list-disc list-inside">
+            <li>Branding</li>
+            <li>strategi</li>
+            <li>Projektledelse</li>
+            <li>Adobepakken</li>
+            <li>Officepakken</li>
+            <li>HTML, CSS og JavaScript</li>
+            <li>Processing</li>
+            <li>Tailwind</li>
+            <li>Figma</li>
+            <li>MM.</li>
+          </ul>
+        </div>
       </div>
       <img src="/media/alberte.webp" alt="" />
-
-      <div class="grid">
-        <h2 class="">Mine kompetencer</h2>
-        <ul class="list-disc list-inside">
-          <li>Branding</li>
-          <li>strategi</li>
-          <li>Projektledelse</li>
-          <li>Adobepakken</li>
-          <li>Officepakken</li>
-          <li>HTML, CSS og JavaScript</li>
-          <li>Processing</li>
-          <li>Tailwind</li>
-          <li>Figma</li>
-          <li>MM.</li>
-        </ul>
-      </div>
-
-      <!-- <div><img src="/media/mig.jpg" alt="" /></div> -->
-      <div>
-        <h2>Interesseret?</h2>
+    </section>
+    <section class="px-56">
+      <div class="grid gap-10 text-center">
+        <h4 class="hover:scale-120">
+          2021: Startede på Multimediedesign på
+          <a
+            href="https://kea.dk/uddannelser/erhvervsakademi/multimediedesigner"
+            >KEA - Københavns Erhvervsakademi</a
+          >
+        </h4>
+        <h4 class="hover:scale-120">
+          2023: Startede som Digital Designer hos
+          <a href="https://www.heagenda.dk/"> Hansen og Ersbøll Agenda</a>
+        </h4>
+        <h4 class="hover:scale-120">
+          2023: Blev færdiguddannet som Multimediedesigner fra
+          <a
+            href="https://kea.dk/uddannelser/erhvervsakademi/multimediedesigner"
+            >KEA - Københavns Erhvervsakademi</a
+          >
+        </h4>
+        <h4 class="hover:scale-120">
+          2023: Startede på Visuel Kommunikation på studieretningen Coded Design
+          på
+          <a
+            href="https://www.dmjx.dk/uddannelser/coded-design?_gl=1*1myq91n*_up*MQ..*_gs*MQ..*_ga*MTY1ODg4MTk3NC4xNzM5OTY3NDI3*_ga_L12XGGD7R8*MTczOTk2NzQyNi4xLjEuMTczOTk2NzQzMi4wLjAuMA..&gclid=EAIaIQobChMI29S14dvPiwMVNQCiAx2P8gprEAAYASAAEgJbvvD_BwE"
+            >DMJX</a
+          >
+        </h4>
+        <h4 class="hover:scale-120">
+          2024: Færdiggjorde 2. semester på Coded Design og droppede ud
+        </h4>
+        <h4 class="hover:scale-120">
+          2024: Startede på Design og Business på specialretningen Creative
+          Branding på
+          <a
+            href="https://kea.dk/uddannelser/top-up/design-business/creative-branding"
+            >KEA - Københavns Erhvervsakademi</a
+          >
+        </h4>
       </div>
     </section>
-
-    <Kontakt></Kontakt>
   </body>
 </template>
 
